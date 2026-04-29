1ce837bf07cf22a442d11eaf91086216b61b2943
Annan An <shy_annan@126.com>
2026-04-29 15:10:35 +0800

    feat: round 2

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
 import { AuthService } from './core/services/auth.service';
 import { JwtResponse } from './core/models/user.model';
 
@@ -19,7 +20,8 @@ import { JwtResponse } from './core/models/user.model';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatMenuModule
+    MatMenuModule,
+    MatDividerModule
   ],
   template: `
     <mat-toolbar color="primary" class="navbar">
